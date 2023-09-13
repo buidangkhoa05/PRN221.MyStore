b8f313486d59118b67aebc6e54b70edaed5d09e4
khoabui <buidangkhoa05@gmail.com>
2023-09-13 12:45:06 +0700

    Add project files.

---
+++ PRN221.Team5.Domain/Common/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace PRN221.Team5.Domain.Common
+{
+    public class AppConfig
+    {
+        public static ConnectionStrings ConnectionStrings { get; set; } = new ConnectionStrings();
+    }
+
+    public class  ConnectionStrings
+    {
+        public string DefaultConnection { get; set; } = "Server=localhost,1433;Database=MyStore;User Id=sa;Password=12345;Trusted_Connection=True;TrustServerCertificate=True;";
+    }
+}
